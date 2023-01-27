c7aed0c7f5241398e71fbe960cac5fe34551a0d9
Enzo <enzo202p@gmail.com>
2023-01-27 17:54:55 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { json } from 'express';
 import boardRouter from './routers/boardRouter';
 import userRouter from './routers/userRouter';
+import cors from "cors";
 
 const server = express();
+server.use(cors());
 server.use(json());
 server.use(userRouter);
 server.use("/board", boardRouter);
+
 
 const port = process.env.PORT || 5000;
 
